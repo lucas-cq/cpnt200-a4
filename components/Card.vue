0a98f32e766c2839bd76017907aef67238a30bde
lucas-cq <lucas.cummings024@gmail.com>
2021-11-22 13:51:11 -0700

    added more images, gallery style

--- components/Card.vue
+++ components/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <figure>
     <h3>{{ info.imgName }}</h3>
-    <nuxt-img :src="info.img" sizes="sm:100vw md:50vw lg:250px" />
+    <nuxt-img :src="info.img" sizes="sm:100vw md:50vw lg:400px" />
       <figcaption>{{ info.desc }}</figcaption>
   </figure>
 </template>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>
+  <div class="gallery-box">
     <main>
       <h2>Card Loop</h2>
       <Card v-for="topic in topics" :key="topic.id" :info="topic" />
     </main>
   </div>
 </template>
+
+<style scoped>
+  figure {
+    margin: auto 0px;
+  }
+  .gallery-box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  h2 {
+    text-align: center;
+  }
+</style>
 
 <script>
 export default {
@@ -19,16 +33,24 @@ export default {
       },
       topics: [
         {
-          imgName: "Lexus IS300",
-          img: "images/sam-warren-aw-ckyl5jsg-unsplash.jpg",
-          desc: "A red lexus IS300",
-          author: "Sam Warren"
+          imgName: "Nissan Silvia's",
+          img: "images/silvia.jpg",
+          desc: "Two Japanese Legends"
         },
         {
-          imgName: "Lexus IS300",
-          img: "images/sam-warren-aw-ckyl5jsg-unsplash.jpg",
-          desc: "A red lexus IS300",
-          author: "Sam Warren"
+          imgName: "Mitsubishi Evo",
+          img: "images/mitevo.jpg",
+          desc: "A Rally God"
+        },
+        {
+          imgName: "Acura Integra",
+          img: "images/teggy.jpg",
+          desc: "The Teenagers Lambo"
+        },
+        {
+          imgName: "Nissan 240sx",
+          img: "images/240sx.jpg",
+          desc: "These used to be cheap"
         }
       ]
     }
