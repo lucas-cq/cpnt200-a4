80e849e401972b3c5079995ff9c8606110ab502c
lucas-cq <lucas.cummings024@gmail.com>
2021-11-22 12:56:20 -0700

    fixed gallery js

---
+++ components/Card.vue
@@ -0,0 +1,13 @@
+<template>
+  <figure>
+    <h3>{{ info.imgName }}</h3>
+    <nuxt-img :src="info.img" sizes="sm:100vw md:50vw lg:250px" />
+      <figcaption>{{ info.desc }}</figcaption>
+  </figure>
+</template>
+
+<script>
+export default {
+  props: ['info']
+}
+</script>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,18 +1,36 @@
 <template>
-  <main>
-
-  </main>
+  <div>
+    <main>
+      <h2>Card Loop</h2>
+      <Card v-for="topic in topics" :key="topic.id" :info="topic" />
+    </main>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      info: {
+      blogInfo: {
         title: "Lucas' Blog",
         author: "Lucas Cummings",
-        desc: "Lucas' kick ass blog"
-      }
+        desc: "Lucas' kick ass blog",
+        
+      },
+      topics: [
+        {
+          imgName: "Lexus IS300",
+          img: "images/sam-warren-aw-ckyl5jsg-unsplash.jpg",
+          desc: "A red lexus IS300",
+          author: "Sam Warren"
+        },
+        {
+          imgName: "Lexus IS300",
+          img: "images/sam-warren-aw-ckyl5jsg-unsplash.jpg",
+          desc: "A red lexus IS300",
+          author: "Sam Warren"
+        }
+      ]
     }
   }
 }
