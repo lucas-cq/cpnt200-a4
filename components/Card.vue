826fe31f0e8dc73a4648784a25b652cd05ab0371
lucas-cq <lucas.cummings024@gmail.com>
2021-11-23 14:41:36 -0700

    more style

--- components/Card.vue
+++ components/Card.vue
@@ -1,10 +1,22 @@
 <template>
   <figure>
-    <h3>{{ info.imgName }}</h3>
-    <nuxt-img :src="info.img" sizes="sm:100vw md:50vw lg:400px" />
+    <h3 class="text-white text-2xl pb-2">{{ info.imgName }}</h3>
+    <nuxt-img class="rounded-sm" :src="info.img" sizes="sm:100vw md:50vw lg:400px" />
       <figcaption>{{ info.desc }}</figcaption>
   </figure>
 </template>
+
+<style scoped>
+
+figure {
+  @apply bg-red-300 mb-8 px-6 py-2 rounded-lg 
+}
+
+figcaption {
+  @apply text-white p-2 text-xl
+}
+
+</style>
 
 <script>
 export default {
--- pages/gallery/index.vue
+++ pages/gallery/index.vue
@@ -3,7 +3,7 @@
       <GlobalNav />
       <h2 class="text-4xl mt-5 text-center mb-8">Car Gallery</h2>
       <div class="gallery-box">
-      <Card class="text-xl" v-for="topic in topics" :key="topic.id" :info="topic" />
+      <Card v-for="topic in topics" :key="topic.id" :info="topic" />
       </div>
       <MyFooter />
     </main>
@@ -11,7 +11,7 @@
 
 <style>
   figure {
-    margin: auto 0px;
+    @apply drop-shadow-2xl
   }
   .gallery-box {
     display: flex;
@@ -45,7 +45,7 @@ export default {
         {
           imgName: "Acura Integra",
           img: "images/teggy.jpg",
-          desc: "The Teenagers Lambo"
+          desc: "A Teenagers Lambo"
         },
         {
           imgName: "Nissan 240sx",
